dc1c6bd22f4ca340d2c45be9f2a43ae10813f816
SubmarineUnderWater <63586591+SubmarineUnderWater@users.noreply.github.com>
2021-08-14 01:56:40 +0900

    게시판

--- frontend/src/page/Board.js
+++ frontend/src/page/Board.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import hamburger from "../css/hamburger.png";
+import magglass from "../css/magglass.png";
+import "../index.css";
 
 const Board = () => {
+    return(
+        <div>
+            <div className="header">
+                <div className="header-left">
+                <button><img className="list" src={hamburger}/></button>
+                <button className="logo">양봉장</button>
+                </div>
+                <div className="header-right">
+                <button><img className="search" src={magglass}/></button>
+                <button className="signIn">로그인</button>
+                <button className="signUp">회원가입</button>
+                </div>
+            </div>
+            <br/><hr size="2"></hr>
 
+            <div className="footer">
+                <p>문의는 포도가 먹고시푼뎅~</p>
+            </div>
+        </div>
+    )
 }
+
+
 
 export default Board;
