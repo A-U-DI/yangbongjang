9ee5bacb23cfc0eecf50a2e87270880dcbeac363
김서영 <ksy990628@gmail.com>
2021-08-13 22:44:43 +0900

    flask - mysql 연동

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+# DB
+import pymysql
+import pandas as pd
+import os
+db = pymysql.connect(host='localhost',
+                     port=3306,
+                     user='root',
+                     passwd='****',
+                     db='ybj',
+                     charset='utf8')
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
