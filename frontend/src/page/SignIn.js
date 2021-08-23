1dadc3cd9bed5a0490cb4ceffa5d86373d8d9bb4
김서영 <ksy990628@gmail.com>
2021-08-23 22:13:48 +0900

    SignIn <-> SignUp

--- frontend/src/page/SignIn.js
+++ frontend/src/page/SignIn.js
@@ -1,38 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import hamburger from "../css/hamburger.png";
 import magglass from "../css/magglass.png";
 import "../index.css";
+import axios from 'axios';
 
 const SignIn = () => {
-return(
-    <div className="header">
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [data,setData] = useState([]);
+    const onClick = () =>{
+        setData([email,password]);
+        axios.post("http://localhost:5000/api/signIn",data).then( request => {
+            console.log(request.data);
+        })}
+    return(
+    <div>
         <div className="header">
             <div className="header-left">
-            <button><img className="list" src={hamburger}/></button>
+            <button><img className="list" src={hamburger}/></button> 
             <button className="logo">양봉장</button>
             </div>
             <div className="header-right">
             <button><img className="search" src={magglass}/></button>
             </div>
         </div>
+        <br/><hr size="2"></hr>
         <div>
-            <div className="title">
-                <h1>회원가입</h1>
-            </div>
+            <div className="title"><h1>로그인</h1></div>
             <div className="form-group">
-                <label for="userEmail">이메일 주소
+                <div className="input">
                     <input type="text" className="form-control" id="userEmail" placeholder="이메일 주소" name="userEmail"/>
-                    </label>
-                    <label for="nickname">닉네임/별명
-                    <input type="text" className="form-control" id="nickname" placeholder="닉네임/별명" name="nickname"/>
-                    </label>
-                    <label for="password">비밀번호
                     <input type="password" className="form-control" id="password" placeholder="비밀번호" name="password"/>
-                    </label>
-                <button type="submit" class="btn btn-warning">제출</button>
                 </div>
+                <div id="btn_group">
+                    <div id="btn1">아이디/비밀번호 찾기
+                </div>
+                <div id="btn2">회원가입</div>
+            </div>
+            <div className="login"><button type="submit" onClick={onClick}>로그인</button></div>
+            </div>
         </div>
-        </div>
+    </div>
 )
 }
 
--- frontend/src/page/SignUp.js
+++ frontend/src/page/SignUp.js
@@ -2,44 +2,34 @@ import React from "react";
 import hamburger from "../css/hamburger.png";
 import magglass from "../css/magglass.png";
 import "../index.css";
-import axios from 'axios';
 
 const SignUp = () => {
 return(
-    <div>
+    <div className="header">
         <div className="header">
             <div className="header-left">
-            <button><img className="list" src={hamburger}/></button> 
+            <button><img className="list" src={hamburger}/></button>
             <button className="logo">양봉장</button>
             </div>
             <div className="header-right">
             <button><img className="search" src={magglass}/></button>
             </div>
         </div>
-        <br/><hr size="2"></hr>
         <div>
             <div className="title">
-                <h1>로그인</h1>
+                <h1>회원가입</h1>
             </div>
             <div className="form-group">
-                <div className="input">
+                <label for="userEmail">이메일 주소
                     <input type="text" className="form-control" id="userEmail" placeholder="이메일 주소" name="userEmail"/>
-                    
+                    </label>
+                    <label for="nickname">닉네임/별명
+                    <input type="text" className="form-control" id="nickname" placeholder="닉네임/별명" name="nickname"/>
+                    </label>
+                    <label for="password">비밀번호
                     <input type="password" className="form-control" id="password" placeholder="비밀번호" name="password"/>
-                    </div>
-                    <div id="btn_group">
-                        
-                            <div id="btn1">아이디/비밀번호 찾기
-                                </div>
-                        
-
-                            <div id="btn2">회원가입
-                            </div>
-                        
-                    </div>
-                    <div className="login">
-                <button type="submit">로그인</button>
-                </div>
+                    </label>
+                <button type="submit" class="btn btn-warning">제출</button>
                 </div>
         </div>
         </div>
