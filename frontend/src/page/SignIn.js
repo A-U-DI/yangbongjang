080ffbe59150dcf65201730e5e7eb1000f025aaa
SubmarineUnderWater <63586591+SubmarineUnderWater@users.noreply.github.com>
2021-08-24 00:07:37 +0900

    회원가입창

--- frontend/src/page/SignIn.js
+++ frontend/src/page/SignIn.js
@@ -5,7 +5,7 @@ import "../index.css";
 
 const SignIn = () => {
 return(
-    <div className="header">
+    <div>
         <div className="header">
             <div className="header-left">
             <button><img className="list" src={hamburger}/></button>
@@ -15,23 +15,28 @@ return(
             <button><img className="search" src={magglass}/></button>
             </div>
         </div>
+        <br/><hr size="2"></hr>
         <div>
+            <div className="input">
             <div className="title">
                 <h1>회원가입</h1>
             </div>
-            <div className="form-group">
-                <label for="userEmail">이메일 주소
+                <div className="foam-group">
+                <label for="userEmail">이메일 주소</label>
                     <input type="text" className="form-control" id="userEmail" placeholder="이메일 주소" name="userEmail"/>
-                    </label>
-                    <label for="nickname">닉네임/별명
+                    
+                    <label for="nickname">닉네임/별명</label>
                     <input type="text" className="form-control" id="nickname" placeholder="닉네임/별명" name="nickname"/>
-                    </label>
-                    <label for="password">비밀번호
+                    
+                    <label for="password">비밀번호</label>
                     <input type="password" className="form-control" id="password" placeholder="비밀번호" name="password"/>
-                    </label>
-                <button type="submit" class="btn btn-warning">제출</button>
+                    
+                    
+                    <div className="login">
+                <button type="submit">회원가입</button></div>
                 </div>
-        </div>
+                </div>
+                </div>
         </div>
 )
 }
