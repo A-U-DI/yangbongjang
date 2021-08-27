a3f06bef4b6ad8655dbd7185e6d6afd64174b972
김서영 <ksy990628@gmail.com>
2021-08-27 19:45:54 +0900

    Join UI

--- frontend/src/page/SignUp.js
+++ frontend/src/page/SignUp.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import hamburger from "../css/hamburger.png";
 import magglass from "../css/magglass.png";
 import "../index.css";
+import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
+    const [email,setEmail] = useState('');
+    const [nickname,setNickname] = useState('');
+    const [password,setPassword] = useState('');
+
+    const onClick = () =>{
+        let JoinForm = new FormData();
+        JoinForm.append('email', email); 
+        JoinForm.append('password', password); 
+
+        axios.post("http://localhost:5000/api/signUp",JoinForm).then(response => { 
+            console.log(response.data);
+        })};    
 return(
-    <div className="header">
+    <div>
         <div className="header">
             <div className="header-left">
             <button><img className="list" src={hamburger}/></button>
@@ -15,25 +29,18 @@ return(
             <button><img className="search" src={magglass}/></button>
             </div>
         </div>
-        <div>
-            <div className="title">
-                <h1>회원가입</h1>
+        <br/><hr size="2"></hr>
+
+        <div className="user-manage">
+            <div className="title">회원가입</div>
+            <div className="user-form">
+                <input type="email" className="input" placeholder="이메일 주소"/>
+                <input type="text" className="input" placeholder="닉네임/별명"/>
+                <input type="password" className="input" placeholder="비밀번호"/>
+                <Link to="/"><button class="login-btn" onClick={onClick}>제출</button></Link>
             </div>
-            <div className="form-group">
-                <label for="userEmail">이메일 주소
-                    <input type="text" className="form-control" placeholder="이메일 주소"/>
-                    </label>
-                    <label for="nickname">닉네임/별명
-                    <input type="text" className="form-control" placeholder="닉네임/별명"/>
-                    </label>
-                    <label for="password">비밀번호
-                    <input type="password" className="form-control" placeholder="비밀번호"/>
-                    </label>
-                <button type="submit" class="btn btn-warning">제출</button>
-                </div>
         </div>
-        </div>
-)
-}
+    </div>
+)}
 
 export default SignUp;
